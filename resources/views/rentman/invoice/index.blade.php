37c8cb2b048dbac9716ba4e90ef7e0a4cb169af3
Jef Melis <jef.melis@melit.be>
2025-11-20 12:01:19 +0100

    added some bootstrap scss variables

--- resources/views/rentman/invoice/index.blade.php
+++ resources/views/rentman/invoice/index.blade.php
@@ -3,40 +3,39 @@
 @section('content')
 <div class="container">
     <div class="p-5 mb-4 bg-body-secondary rounded-3 ">
-        <div class="my-2">
-            add code for showing a list of invoices
-
-
-            <table class="table table-striped">
-                <thead>
+        <h1>Invoices</h1>
+        <div class="my-2 table-responsive">
+            <div class="table-responsive">
+                <table class="table table-sm table-bordered table-hover table-striped">
+                    <thead class="table-dark">
+                        <tr>
+                            <th class="text-center" scope="col">id</th>
+                            <th class="text-center" scope="col">number</th>
+                            <th scope="col">displayname</th>
+                            <th scope="col">customer</th>
+                            <th scope="col">accountmanager</th>
+                        </tr>
+                    </thead>
+                    @foreach($invoices as $invoice)
                     <tr>
-                        <th scope="col">id</th>
-                        <th scope="col">number</th>
-                        <th scope="col">displayname</th>
-                        <th scope="col">customer</th>
-                        <th scope="col">accountmanager</th>
+                        <td class="text-center">
+                            <a href="{{ route('invoices.show', $invoice->id) }}">
+                                <i class="bi bi-pencil-fill"></i>
+                            </a>
+                        </td>
+                        <td class="text-center">{{ $invoice->number }}</td>
+                        <td>{{ $invoice->displayname }}</td>
+                        <td>{{ $invoice->customer }}</td>
+                        <td>{{ $invoice->account_manager }}</td>
+                        {{-- <td>{{ $invoice }}</td> --}}
                     </tr>
-                </thead>
-                @foreach($invoices as $invoice)
-                <tr>
-                    <td>
-                        <a href="{{ route('invoices.show', $invoice->id) }}">
-                            <i class="bi bi-pencil-fill"></i>
-                        </a>
-                    </td>
-                    <td>{{ $invoice->number }}</td>
-                    <td>{{ $invoice->displayname }}</td>
-                    <td>{{ $invoice->customer }}</td>
-                    <td>{{ $invoice->account_manager }}</td>
-                    {{-- <td>{{ $invoice }}</td> --}}
-                </tr>
-                @endforeach
-            </table>
+                    @endforeach
+                </table>
 
-            <div class="mt-4">
-                {{ $invoices->links() }}
+                <div class="mt-4">
+                    {{ $invoices->links() }}
+                </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
--- resources/views/rentman/project/index.blade.php
+++ resources/views/rentman/project/index.blade.php
@@ -1,41 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
+<div class="container">
     <div class="mx-2 p-3 bg-light rounded-3">
         <H1>Projects</H1>
 
-        <table class="table table-striped">
-            <thead></thead>
-            <tbody>
-                <tr>
-                    <td colspan="99">{{ $projects[0] }} </td>
-                </tr>
-                @foreach($projects as $key => $project)
-                <tr>
-                    <td>
-                        <a href="{{ route('projects.show', $project->id) }}">
-                            <i class="bi bi-pencil-fill"></i>
-                        </a>
-                    </td>
-                    <td>{{ $project->id }} </td>
-                    <td>{{ $project->name }} </td>
-                    <td>{{ $project->displayname }} </td>
-                    <td>{{ $project->project_type }} </td>
-
-                    <td>{{ $project->location }} </td>
-                    <td>{{ $project->planperiod_start }} </td>
-                    <td>{{ $project->planperiod_end }} </td>
-                    <td>{{ $project->usageperiod_start }} </td>
-                    <td>{{ $project->usageperiod_end }} </td>
-                    <td>{{ $project->equipment_period_from }} </td>
-                    <td>{{ $project->equipment_period_to }} </td>
-                    <td>{{ $project->_created }} </td>
-                    <td>{{ $project->_updated }} </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table table-sm table-bordered table-hover table-striped">
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">xxxx</th>
+                        <th scope="col">number</th>
+                        <th scope="col">name</th>
+                        <th scope="col">Reference</th>
+                        <th scope="col">project type</th>
+                        <th scope="col">location</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($projects as $key => $project)
+                    <tr>
+                        <td class="text-center">
+                            <a href="{{ route('projects.show', $project->id) }}">
+                                <i class="bi bi-pencil-fill"></i>
+                            </a>
+                        </td>
+                        <td class="text-center">{{ $project->number }} </td>
+                        <td>{{ $project->name }} </td>
+                        <td>{{ $project->reference }} </td>
+                        <td>{{ $project->project_type }} </td>
+                        <td>{{ $project->location }} </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="mt-4">
+            {{ $projects->links() }}
+        </div>
     </div>
 </div>
 @endsection
