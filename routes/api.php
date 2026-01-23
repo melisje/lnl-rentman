df56c0c0454b1d7d9f1761d4f1578ce52fc52282
Jef Melis <jef.melis@melit.be>
2026-01-23 15:11:38 +0100

    added get route for webhook

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,4 @@ Route::get('/user', function (Request $request) {
 
 
 Route::post('/webhook/rentman', [WebhookController::class, 'handle']);
+Route::get('/webhook/rentman', [WebhookController::class, 'handle']);
