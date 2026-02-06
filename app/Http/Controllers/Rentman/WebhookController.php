78476d313b9fa647d598632574ddd4784545e15a
Jef Melis <jef.melis@melit.be>
2026-02-06 23:59:08 +0100

    Cleanup controller

--- app/Http/Controllers/Rentman/WebhookController.php
+++ app/Http/Controllers/Rentman/WebhookController.php
@@ -10,15 +10,11 @@ use App\Models\Rentman\WebhookCall;
 
 class WebhookController extends Controller
 {
-    public array $payload;
-
     //
     public function handle(Request $request)
     {
-        // 1. Log incoming data (Usefull for debugging)
+        // Log incoming data (Usefull for debugging)
         Log::info('Webhook ontvangen:',$request->all());
-
-        $this->payload = $request->all();
 
         // Signature verification
         $secret = config('services.webhook.secret'); // the secret key
@@ -33,30 +29,7 @@ class WebhookController extends Controller
         // }
 
         // Dispatch job and immediately return response
-        // ProcessWebhookRentman::dispatch($request->all());
-        ProcessWebhookRentman::dispatch($this->payload);
-
-
-        // $eventype = $this->payload['eventType'] ?? 'unknown';
-        // $itemtype = $this->payload['itemType'] ?? 'unknown';
-        // $items = $this->payload['items'];
-        // $eventdate = $this->payload['eventDate'];
-
-        // Log::info("Eventype: $eventype");
-        // Log::info("Itemtype: $itemtype");
-        // Log::info("Items: " . json_encode($items));
-        // Log::info("Eventdate: $eventdate");
-
-        // $wbc = new WebhookCall;
-        // $wbc->account = $this->payload['account'] ?? 'unknown';
-        // $wbc->user = $this->payload['user']['id'] ?? 'unknown';
-        // $wbc->eventType = $this->payload['eventType'] ?? 'unknown';
-        // $wbc->itemType = $this->payload['itemType'] ?? 'unknown';
-        // $wbc->items = json_encode($this->payload['items']);
-        // $wbc->eventDate = $this->payload['eventDate'];
-
-        // $wbc->save();
-
+        ProcessWebhookRentman::dispatch($request->all());
 
         // ALWAYS return a 200 OK code, asap
         return response()->json(['status' => 'received']);
