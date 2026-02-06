0525bde1431a986adcb1aa3ac26778b7d0f4abd5
= <jef.melis@melit.be>
2026-02-06 21:21:22 +0000

    implemented queue worker

--- app/Http/Controllers/Rentman/WebhookController.php
+++ app/Http/Controllers/Rentman/WebhookController.php
@@ -6,15 +6,19 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Jobs\ProcessWebhookRentman;
+use App\Models\Rentman\WebhookCall;
 
 class WebhookController extends Controller
 {
+    public array $payload;
 
     //
     public function handle(Request $request)
     {
         // 1. Log incoming data (Usefull for debugging)
-        Log::info('Webhok ontvangen:',$request->all());
+        Log::info('Webhook ontvangen:',$request->all());
+
+        $this->payload = $request->all();
 
         // Signature verification
         $secret = config('services.webhook.secret'); // the secret key
@@ -24,12 +28,35 @@ class WebhookController extends Controller
         $payload = $request->getContent();
         $expected = hash_hmac('sha256', $payload, $secret);
 
-        if (!hash_equals($expected, $signature)) {
-            abort(403, 'Unauthorized webhook signature.');
-        }
+        // if (!hash_equals($expected, $signature)) {
+        //     abort(403, 'Unauthorized webhook signature.');
+        // }
 
         // Dispatch job and immediately return response
-        ProcessWebhookRentman::dispatch($request->all());
+        // ProcessWebhookRentman::dispatch($request->all());
+        ProcessWebhookRentman::dispatch($this->payload);
+
+
+        // $eventype = $this->payload['eventType'] ?? 'unknown';
+        // $itemtype = $this->payload['itemType'] ?? 'unknown';
+        // $items = $this->payload['items'];
+        // $eventdate = $this->payload['eventDate'];
+
+        // Log::info("Eventype: $eventype");
+        // Log::info("Itemtype: $itemtype");
+        // Log::info("Items: " . json_encode($items));
+        // Log::info("Eventdate: $eventdate");
+
+        // $wbc = new WebhookCall;
+        // $wbc->account = $this->payload['account'] ?? 'unknown';
+        // $wbc->user = $this->payload['user']['id'] ?? 'unknown';
+        // $wbc->eventType = $this->payload['eventType'] ?? 'unknown';
+        // $wbc->itemType = $this->payload['itemType'] ?? 'unknown';
+        // $wbc->items = json_encode($this->payload['items']);
+        // $wbc->eventDate = $this->payload['eventDate'];
+
+        // $wbc->save();
+
 
         // ALWAYS return a 200 OK code, asap
         return response()->json(['status' => 'received']);
--- app/Jobs/ProcessWebhookRentman.php
+++ app/Jobs/ProcessWebhookRentman.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Log;
+use App\Models\Rentman\WebhookCall;
 
 class ProcessWebhookRentman implements ShouldQueue
 {
@@ -31,6 +32,17 @@ class ProcessWebhookRentman implements ShouldQueue
         $itemtype = $this->payload['itemType'] ?? 'unknown';
         $items = $this->payload['items'];
         $eventdate = $this->payload['eventDate'];
+
+
+        $wbc = new WebhookCall;
+        $wbc->account = $this->payload['account'] ?? 'unknown';
+        $wbc->user = $this->payload['user']['id'] ?? 'unknown';
+        $wbc->eventType = $this->payload['eventType'] ?? 'unknown';
+        $wbc->itemType = $this->payload['itemType'] ?? 'unknown';
+        $wbc->items = json_encode($this->payload['items']);
+        $wbc->eventDate = $this->payload['eventDate'];
+
+        $wbc->save();
 
     }
 }
