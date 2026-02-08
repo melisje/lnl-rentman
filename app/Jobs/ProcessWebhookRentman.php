a951e972b6db721743f05f58685c830673160865
Jef Melis <jef.melis@melit.be>
2026-02-08 12:34:37 +0100

    Finalised fase one of receiving, verifiieng  and processing received webhook calls

--- app/Jobs/ProcessWebhookRentman.php
+++ app/Jobs/ProcessWebhookRentman.php
@@ -11,32 +11,60 @@ class ProcessWebhookRentman implements ShouldQueue
 {
     use Queueable;
 
-    public array $payload;
+    public array $data;
 
     /**
      * Create a new job instance.
      */
-    public function __construct(array $payload)
+    public function __construct(array $data)
     {
-        $this->payload = $payload;
+        $this->data = $data;
     }
 
     /**
      * Execute the job.
+     * 1. Store the webhook call in the database
+     * 2. Notify listeners for the received events
      */
     public function handle(): void
     {
-        Log::info('Processing webhook payload:', $this->payload);
+        // Explode received data array
+        $payload = $this->data['payload'];
+        $headers = $this->data['headers'];
+        $rawcontent = $this->data['rawcontent'];
+        $ip = $this->data['ip'];
 
+        Log::info('Processing webhook payload:', $payload);
+
+        // Make sure user exists in crew table from given account
+        $account = $payload['account'];
+        $user = $payload['user']['id'];
+        $this->sync_crew_user($account,$user);
+
+        // store the call info in the database
         $wbc = new WebhookCall;
-        $wbc->payload = json_encode($this->payload);
-        $wbc->account = $this->payload['account'] ?? 'unknown';
-        $wbc->user = $this->payload['user']['id'] ?? 'unknown';
-        $wbc->eventType = $this->payload['eventType'] ?? 'unknown';
-        $wbc->itemType = $this->payload['itemType'] ?? 'unknown';
-        $wbc->items = json_encode($this->payload['items']);
-        $wbc->eventDate = $this->payload['eventDate'];
+        // $wbc->payload = json_encode($payload);
+        $wbc->payload = $rawcontent;
+        $wbc->account = $account;
+        $wbc->user = $user;
+        $wbc->ip = $ip;
+        $wbc->headers = $headers;
+        $wbc->eventType = $payload['eventType'] ?? 'unknown';
+        $wbc->itemType = $payload['itemType'] ?? 'unknown';
+        $wbc->items = json_encode($payload['items']);
+        $wbc->eventDate = $payload['eventDate'];
         $wbc->save();
 
+        // TODO: process - inform interested listeners about new webhook call
+
+    }
+
+    /**
+     * To call a rentman API endpoint, we must have the proper api key that belongs to the correct account
+     */
+    public function sync_crew_user($account,$user)
+    {
+        $rmapiurl = "";
+        $url = "$account.$rmapiurl";
     }
 }
