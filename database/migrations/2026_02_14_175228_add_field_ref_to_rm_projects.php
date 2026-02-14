43970d1b1b7bcb6685142b115792e89c95f418d7
Jef Melis <jef.melis@melit.be>
2026-02-14 21:06:26 +0100

    added field for ref and status

---
+++ database/migrations/2026_02_14_175228_add_field_ref_to_rm_projects.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('rm_projects', function (Blueprint $table) {
+            $table->string('ref')->after('rm_id')->nullable();
+            $table->string('status')->nullable()->after('modified');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('rm_projects', function (Blueprint $table) {
+            //
+            $table->dropColumn('status');
+            $table->dropColumn('ref');
+        });
+    }
+};
