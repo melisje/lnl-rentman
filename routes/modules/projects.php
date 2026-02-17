dda6b14c77b7f5489f340a789a65dbff56f24e6e
Jef Melis <jef.melis@melit.be>
2026-02-17 09:24:08 +0100

    Refactor ProjectController to llstageservice namespace and update routes accordingly

--- routes/modules/projects.php
+++ routes/modules/projects.php
@@ -1,22 +1,10 @@
     <?php
 
     use Illuminate\Support\Facades\Route;
-    use App\Http\Controllers\rentman\InvoiceController;
-    use App\Http\Controllers\Rentman\ProjectController;
+    use App\Http\Controllers\Rentman\llstageservice\ProjectController;
 
     Route::middleware(['can:access-projects'])->group(function()
     {
-      // Specific, non-resource routes
-      Route::prefix('projects')->group(function () {
-        Route::controller(ProjectController::class)
-          ->name('projects.')
-          ->group(function () {
-            Route::get('/fetch', 'fetch')->name('fetch');
-            Route::post('/fetch', 'fetch')->name('fetch');
-          });
-      });
-
-      // Project resourse routes
-      Route::resource('projects', ProjectController::class);
+        Route::get('/{account}/projects', [ProjectController::class, 'index'])->name('projects.index');
     });
 
