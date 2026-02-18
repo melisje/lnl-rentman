a60977f97effa0e1a307258765e14ebe78cfad0d
Jef Melis <jef.melis@melit.be>
2026-02-18 17:32:40 +0100

    Add CustomFieldMapping model, controller, and migration for custom field mappings

---
+++ app/Http/Controllers/CustomFieldMappingController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rentman\CustomFieldMapping;
+use Illuminate\Http\Request;
+
+class CustomFieldMappingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CustomFieldMapping $customFieldMapping)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CustomFieldMapping $customFieldMapping)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CustomFieldMapping $customFieldMapping)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CustomFieldMapping $customFieldMapping)
+    {
+        //
+    }
+}
