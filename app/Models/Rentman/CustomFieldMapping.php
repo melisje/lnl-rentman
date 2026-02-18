b8469b4ea136117f18cc5b28098cbff217c854bd
Jef Melis <jef.melis@melit.be>
2026-02-18 22:25:03 +0100

    Add CustomFieldMapping and Mapping models with relationships; create migration to add project_manager field to rm_projects

--- app/Models/Rentman/CustomFieldMapping.php
+++ app/Models/Rentman/CustomFieldMapping.php
@@ -3,8 +3,22 @@
 namespace App\Models\Rentman;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CustomFieldMapping extends Model
 {
     //
+    protected $table = 'rm_customfield_mappings';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    protected $guarded = [];
+    // protected $fillable = ['rm_id', 'account', 'displayname'];
+
+    /**
+     * Get the custom fields for the mapping.
+     */
+    public function mapping(): BelongsTo
+    {
+        return $this->belongsTo(Mapping::class, 'mapping_id', 'id');
+    }
 }
---
+++ database/migrations/2026_02_18_211851_add_fields_to_rm_projects.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('rm_projects', function (Blueprint $table)
+        {
+            //
+            $table->string('project_manager')->nullable()->after('account_manager');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('rm_projects', function (Blueprint $table)
+        {
+            $table->dropColumn('project_manager');
+        });
+    }
+};
