6559578f6d0c24ab159ff94813ab453f2f1842fe
Jef Melis <jef.melis@melit.be>
2026-02-20 13:25:49 +0100

    Wrap crew model update in a conditional check to ensure crew data exists before syncing

--- app/Jobs/ProcessWebhookRentman.php
+++ app/Jobs/ProcessWebhookRentman.php
@@ -56,38 +56,40 @@ class ProcessWebhookRentman implements ShouldQueue
 
         $crewdata = $rentmanApiService->sync_crew_user($whc->account,$user);
 
-        // update or create crew model
-        $crew = Crew::upsert(
-            [
-                'account' => $whc->account,
-                'rm_id' => $whc->user,
-                'created' => $crewdata['created'],
-                'modified' => $crewdata['modified'],
-                'creator' => $crewdata['creator'],
-                'displayname' => $crewdata['displayname'],
-                'updateHash' => $crewdata['updateHash'],
-                'folder' => $crewdata['folder'],
-                'street' => $crewdata['street'],
-                'housenumber' => $crewdata['housenumber'],
-                'city' => $crewdata['city'],
-                'postal_code' => $crewdata['postal_code'],
-                'addressline2' => $crewdata['addressline2'],
-                'firstname' => $crewdata['firstname'],
-                'middle_name' => $crewdata['middle_name'],
-                'lastname' => $crewdata['lastname'],
-                'email' => $crewdata['email'],
-                'active' => $crewdata['active'],
-                'tags' => $crewdata['tags'],
-                'custom' => json_encode($crewdata['custom']),
-            ],
-            [
-                'account' => $whc->account,
-                'rm_id' => $whc->user
-            ]
-        );
+        if($crewdata)
+        {
+            // update or create crew model
+            $crew = Crew::upsert(
+                [
+                    'account' => $whc->account,
+                    'rm_id' => $whc->user,
+                    'created' => $crewdata['created'],
+                    'modified' => $crewdata['modified'],
+                    'creator' => $crewdata['creator'],
+                    'displayname' => $crewdata['displayname'],
+                    'updateHash' => $crewdata['updateHash'],
+                    'folder' => $crewdata['folder'],
+                    'street' => $crewdata['street'],
+                    'housenumber' => $crewdata['housenumber'],
+                    'city' => $crewdata['city'],
+                    'postal_code' => $crewdata['postal_code'],
+                    'addressline2' => $crewdata['addressline2'],
+                    'firstname' => $crewdata['firstname'],
+                    'middle_name' => $crewdata['middle_name'],
+                    'lastname' => $crewdata['lastname'],
+                    'email' => $crewdata['email'],
+                    'active' => $crewdata['active'],
+                    'tags' => $crewdata['tags'],
+                    'custom' => json_encode($crewdata['custom']),
+                ],
+                [
+                    'account' => $whc->account,
+                    'rm_id' => $whc->user
+                ]
+            );
 
-        Log::info("Crew $whc->id synced for account $whc->account");
-
+            Log::info("Crew $whc->id synced for account $whc->account");
+        }
         // We are now sure that the user is also in the crew
         // table and we can save the WebhookCall model to db
         $whc->save();
