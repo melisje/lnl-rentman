4ebb008ad79c3e3a41c9261f00573a37ba9aeb3e
Jef Melis <jef.melis@melit.be>
2026-02-20 12:49:03 +0100

    Add mappings relationship to CustomField model for CustomFieldMapping

--- app/Models/Rentman/CustomField.php
+++ app/Models/Rentman/CustomField.php
@@ -5,6 +5,7 @@ namespace App\Models\Rentman;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\Rentman\Account;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CustomField extends Model
 {
@@ -17,8 +18,16 @@ class CustomField extends Model
     /**
      * Relatie naar het Rentman Account model.
      */
-public function rentmanAccount(): BelongsTo
+    public function rentmanAccount(): BelongsTo
     {
         return $this->belongsTo(Account::class, 'account', 'account');
+    }
+
+    /**
+     * Relatie naar de CustomFieldMapping model.
+     */
+    public function mappings(): HasMany
+    {
+        return $this->hasMany(CustomFieldMapping::class, 'customfield_id', 'id');
     }
 }
