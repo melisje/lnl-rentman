3cce513578b39f79cc9433a3163c13a3094aff5b
Jef Melis <jef.melis@melit.be>
2026-03-05 10:54:38 +0100

    Added datatables

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,17 @@
 import './bootstrap';
-
-// Add Tabulator to the global window object so it can be used in your scripts
+import $ from 'jquery';
+import DataTable from 'datatables.net-bs5';
 import { TabulatorFull as Tabulator } from 'tabulator-tables';
+
+// STAP A: Maak jQuery als ALLEREERSTE globaal beschikbaar
+window.$ = window.jQuery = $;
+// De meest robuuste manier om jQuery globaal te maken
+// Object.assign(window, { $: $, jQuery: $ });
+
+// STAP B: Koppel de plugins handmatig als de automatische koppeling faalt
+// Registreer de plugin expliciet
+// DataTable(window, $);
+
+// STAP C: Andere globale variabelen
 window.Tabulator = Tabulator;
 
-import 'tabulator-tables/dist/css/tabulator_bootstrap5.min.css';
