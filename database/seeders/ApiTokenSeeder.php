43c1fe8ed8168d08806d2df33066ecd25504a995
Jef Melis <jef.melis@melit.be>
2026-04-02 12:32:18 +0200

    Refactor ApiToken naar Account in Seeder

--- database/seeders/ApiTokenSeeder.php
+++ database/seeders/ApiTokenSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Rentman\ApiToken;
+use App\Models\Rentman\Account;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,7 +14,7 @@ class ApiTokenSeeder extends Seeder
     public function run(): void
     {
         // add api_tokens for account llstageservice and ledvisions as known on 2026-02-07
-        $token = ApiToken::updateOrCreate(
+        $token = Account::updateOrCreate(
             ["account" => "llstageservice"],
             [
                 "api_token"     => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZWRld2Vya2VyIjo1OTQsImFjY291bnQiOiJsbHN0YWdlc2VydmljZSIsImNsaWVudF90eXBlIjoib3BlbmFwaSIsImNsaWVudC5uYW1lIjoib3BlbmFwaSIsImV4cCI6MTg1NjE3MzkyNCwiaXNzIjoie1wibmFtZVwiOlwiYmFja2VuZFwiLFwidmVyc2lvblwiOlwiNC41ODQuMC4xXCJ9IiwiaWF0IjoxNjk4MzIxMTI0fQ.IrZfEugnBoo0DV3e1DGViJp8_fNkI7owasG5zZH-xis",
@@ -22,7 +22,7 @@ class ApiTokenSeeder extends Seeder
                 "url" => "https://api.rentman.net"
             ]);
 
-        $token = ApiToken::updateOrCreate(
+        $token = Account::updateOrCreate(
             ["account" => "ledvisions"],
             [
                 "api_token"     => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZWRld2Vya2VyIjoyMzUsImFjY291bnQiOiJsZWR2aXNpb25zIiwiY2xpZW50X3R5cGUiOiJvcGVuYXBpIiwiY2xpZW50Lm5hbWUiOiJvcGVuYXBpIiwiZXhwIjoyMDY1OTQzODQ2LCJpc3MiOiJ7XCJuYW1lXCI6XCJiYWNrZW5kXCIsXCJ2ZXJzaW9uXCI6XCI0Ljc1NC4wLjFcIn0iLCJpYXQiOjE3NTA0MTEwNDZ9.Hhf8qpROUkJbLIlpfcGGKPVAVda-ksBPGszCaaB0HPc",
@@ -30,7 +30,7 @@ class ApiTokenSeeder extends Seeder
                 "url" => "https://api.rentman.net"
             ]);
 
-        $token = ApiToken::updateOrCreate(
+        $token = Account::updateOrCreate(
             ["account" => "rentmantest"],
             [
                 "api_token"     => "",
