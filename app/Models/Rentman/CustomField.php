2e5a929e16344049197b3505bf150adc261d6b85
Jef Melis <jef.melis@melit.be>
2026-04-15 15:05:52 +0200

    Added global scope to filter current account of web user

--- app/Models/Rentman/CustomField.php
+++ app/Models/Rentman/CustomField.php
@@ -5,6 +5,7 @@ namespace App\Models\Rentman;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\Rentman\Account;
+use App\Scopes\AccountScope;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CustomField extends Model
@@ -13,6 +14,18 @@ class CustomField extends Model
     protected $primaryKey = 'id';
     public $incrementing = true;
     protected $guarded = [];
+
+
+    /**
+     * Boot the model and apply the global scope.
+     */
+    protected static function booted()
+    {
+        // Filter all queries automatically based on the current account.
+        // If you do want to run a query without this scope, you can
+        // use: Project::withoutGlobalScope(AccountScope::class)->get();
+        static::addGlobalScope(new AccountScope);
+    }
 
 
     /**
