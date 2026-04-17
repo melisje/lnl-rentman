5141df2f174ea1736d2138b14160e03db76e64c6
Jef Melis <jef.melis@melit.be>
2026-04-17 14:34:07 +0200

    Added test2 for as demo implementation for project_types_application mappings

--- app/Http/Controllers/testtom/TestJefController.php
+++ app/Http/Controllers/testtom/TestJefController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\testtom;
 
 use App\Http\Controllers\Controller;
+use App\Models\Rentman\Account;
+use App\Models\Rentman\Application;
 use App\Models\Rentman\Project;
+use App\Models\Rentman\ProjectType;
 use App\Scopes\AccountScope;
 use Illuminate\Support\Facades\DB;
 
@@ -24,6 +27,28 @@ class TestJefController extends Controller
             'models' => $projects,
         ]);
 
+
+    }
+
+    /**
+     * Find projects mapped to a specific appliction
+     *
+     */
+    public function test2()
+    {
+        // Find the project that we are working on
+        $app = Application::first();
+
+
+        // Find all projects that are related to this application based on the
+        // table rm_project_type_application_mapping.
+
+        $accounts = ['llstageservice', 'ledvisions'];
+        foreach ($accounts as $account)
+        {
+            $result = $app->projects($account)->count();
+            dump("Account: $account, project count: $result");
+        }
 
     }
 
--- routes/modules/testtom.php
+++ routes/modules/testtom.php
@@ -26,6 +26,7 @@ use Illuminate\Support\Facades\Route;
        */
       // Test route to check if the AccountScope is working correctly on the Project model
       Route::get('testjef1', [TestJefController::class, 'test1'])->name('projects_with_account_scope');
+      Route::get('testjef2', [TestJefController::class, 'test2'])->name('projecttypes');
 
 
 
