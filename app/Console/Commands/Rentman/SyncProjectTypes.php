2502a1ef8fb54fd53aa2a66f5dc64f023e481708
Jef Melis <jef.melis@melit.be>
2026-04-17 14:33:18 +0200

    Added functionality to update the project_type_id of projects after syncing project_types with rentman

--- app/Console/Commands/Rentman/SyncProjectTypes.php
+++ app/Console/Commands/Rentman/SyncProjectTypes.php
@@ -4,6 +4,9 @@ namespace App\Console\Commands\Rentman;
 
 use Illuminate\Console\Command;
 use App\Models\Rentman\Account;
+use App\Models\Rentman\Project;
+use App\Models\Rentman\ProjectType;
+use App\Scopes\AccountScope;
 use App\Services\Rentman\Api\ProjectTypeFetcher;
 
 class SyncProjectTypes extends Command
@@ -78,6 +81,10 @@ class SyncProjectTypes extends Command
         }
 
         $this->info("\n✅ ProjectTypes synchronisation process finished. We created or updated {$this->totalItems} project types over all accounts.");
+
+        // Update Projects with project types id
+        $this->updateProject();
+
         return Command::SUCCESS;
     }
 
@@ -94,5 +101,38 @@ class SyncProjectTypes extends Command
 
         $this->nrOfItems  += count($items);
         $this->totalItems += count($items);
+    }
+
+    public function updateProject()
+    {
+        $this->info("++++-> Updating projects with project type ids.");
+        // List all accounts in db
+        $accounts = Account::pluck('account')->toArray();
+
+        // Loop through accounts and update projects with project type ids based on related ProjectType records
+        foreach ($accounts as $account) {
+            $this->info("   ~~~> Processing account: $account");
+
+            // Get all projecttypes for the current account and get their rm_ids
+            $projectTypes =    ProjectType::withoutGlobalScope(AccountScope::class)
+                ->where('account', $account)
+                ->get();
+
+            // Loop through the rm_ids and update the projects with the corresponding project type ids
+            foreach ($projectTypes as $projectType) {
+                // Build the project type path based on the rm_id of the project type
+                $projectTypePath = '/projecttypes/' . $projectType->rm_id;
+                $this->info("      ~~~> Updating projects for account: $account with project type path: $projectTypePath to have project type id: $projectType->id");
+
+                // Update the projects with the corresponding project type ids based on the project
+                // type path and account, while ignoring the global scope to ensure we can update
+                // all relevant projects
+                Project::withoutGlobalScope(AccountScope::class)
+                    ->where('account', $account)
+                    ->where('project_type', $projectTypePath)
+                    ->update(['project_type_id' => $projectType->id]);
+            }
+        }
+
     }
 }
