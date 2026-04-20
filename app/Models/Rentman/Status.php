b18085faa7ab65cc012336bfb901f2428f960d01
Jef Melis <jef.melis@melit.be>
2026-04-20 16:14:50 +0200

    Added booted and casts methods

--- app/Models/Rentman/Status.php
+++ app/Models/Rentman/Status.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Rentman;
 
+use App\Scopes\AccountScope;
 use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
@@ -12,4 +13,30 @@ class Status extends Model
     [
         'id'
     ];
+
+    /**
+     * De casts die moeten worden toegepast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime', // Optioneel: direct formatteren
+            'modified_at' => 'datetime', // Optioneel: direct formatteren
+            'created' => 'datetime', // Dit cast de kolom naar Carbon
+            'modified' => 'datetime', // Dit cast de kolom naar Carbon
+        ];
+    }
+
+    /**
+     * Boot the model and apply the global scope.
+     */
+    protected static function booted()
+    {
+        // Filter all queries automatically based on the current account.
+        // If you do want to run a query without this scope, you can
+        // use: Project::withoutGlobalScope(AccountScope::class)->get();
+        static::addGlobalScope(new AccountScope);
+    }
 }
