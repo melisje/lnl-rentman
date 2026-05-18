1631f89d07919c284eddbb73e5ff212bec0de267
Jef Melis <jef.melis@melit.be>
2026-05-18 16:04:47 +0200

    typo fix

--- resources/views/layouts/nav_production.blade.php
+++ resources/views/layouts/nav_production.blade.php
@@ -5,18 +5,7 @@
     </a>
     <ul class="dropdown-menu">
         @include('layouts.nav_projects')
-        @include('layouts.nav_projects')
         @include('layouts.nav_checklists')
-        <li>
-            <hr class="dropdown-divider">
-        </li>
-
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li>
-            <hr class="dropdown-divider">
-        </li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
     </ul>
 </li>
 @endcan
