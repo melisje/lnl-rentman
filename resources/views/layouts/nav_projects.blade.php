ec41f3785c2805ff5ac5d8e3f67de509ab4d2ca6
Jef Melis <jef.melis@melit.be>
2026-05-29 09:55:44 +0200

    add timeregistrations to menu Production

--- resources/views/layouts/nav_projects.blade.php
+++ resources/views/layouts/nav_projects.blade.php
@@ -3,5 +3,9 @@
     <hr class="dropdown-divider">
 </li>
 <li><a class="dropdown-item" href="{{ route('rentman.projects.index', ['account' => 'llstageservice']) }}">{{ __('projects')}}</a></li>
+<li><a class="dropdown-item" href="{{ route('production.project.timeregistration.index', ['account' => 'llstageservice']) }}">{{ __('Timeregistrations')}}</a></li>
+<li>
+    <hr class="dropdown-divider">
+</li>
 <li><a class="dropdown-item" href="{{ route('testtom.weekoverzicht', ['account' => 'llstageservice']) }}" target="_blank">{{ __('weekoverview')}}</a></li>
 @endcan
