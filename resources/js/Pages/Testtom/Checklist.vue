75194f9b4978252d66da7ee4b5e5248781dcd239
TomTech Productions <37588160+tomschelles@users.noreply.github.com>
2026-06-01 21:22:40 +0200

    aanpassingen layout

--- resources/js/Pages/Testtom/Checklist.vue
+++ resources/js/Pages/Testtom/Checklist.vue
@@ -1,3 +1,6 @@
 <template>
-    Hello Checklist
+    <h1 class="fw-bold">Checklist</h1>
+    <input type="button" class="btn btn-orange m-2" value="Nieuwe Checklist">
+    <input type="button" class="btn btn-orange m-2" value="Nieuwe Template">
+    
 </template>   
